7c2f6c6d345682c0c166682cd8dc38eb2a3e5f16
dhl8282 <donghun.lee7@gmail.com>
2014-05-12 01:49:42 +0900

    Save full image to hobbit folder
    https://app.asana.com/0/11989140532570/12230414644076

---
+++ src/com/example/hobbit/Constants.java
@@ -0,0 +1,6 @@
+package com.example.hobbit;
+
+public class Constants {
+    public static final String PICTURE_DIR = "/Pictures/";
+    public static final String HOBBIT_DIR = "/Hobbit/";
+}
--- src/com/example/hobbit/MainMenuActivity.java
+++ src/com/example/hobbit/MainMenuActivity.java
@@ -1,20 +1,32 @@
 package com.example.hobbit;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenuActivity extends Activity {
 
     private static final String TAG = "hobbit" + MainMenuActivity.class.getSimpleName();
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private final int MAX_IMAGE_DIMENSION = 300;
+    static final int REQUEST_TAKE_PHOTO = 1;
     private Button buttonCreateMission, buttonEnterMission;
+    private String mCurrentPhotoPath;
+    private Uri mImageUri;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -44,33 +56,84 @@ public class MainMenuActivity extends Activity {
 	}
 
 	private void addEnterMission() {
-	    final Intent intent = new Intent(this, EnterMissionActivity.class);
+	    final Intent enterMissionIntent = new Intent(this, EnterMissionActivity.class);
 	    buttonEnterMission = (Button) findViewById(R.id.buttonEnterMission);
 	    buttonEnterMission.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startActivity(intent);
+                startActivity(enterMissionIntent);
             }
         });
 	}
 
+	private File createImageFile() throws IOException {
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    File file = new File(Environment.getExternalStorageDirectory()
+	                    + File.separator
+	                    + Constants.PICTURE_DIR
+	                    + Constants.HOBBIT_DIR + timeStamp + ".jpg");
+	    mCurrentPhotoPath = "file:" + file.getAbsolutePath();
+	    return file;
+	}
+
 	private void dispatchTakePictureIntent() {
 	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    //Ensure that there's a camera activity to handle the intent
+	    // Ensure that there's a camera activity to handle the intent
 	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            // Error occurred while creating the File
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	            mImageUri = Uri.fromFile(photoFile);
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+	        }
 	    }
 	}
 
+	public Bitmap grabImage()
+	{
+	    this.getContentResolver().notifyChange(mImageUri, null);
+	    ContentResolver cr = this.getContentResolver();
+	    Bitmap bitmap, scaledBitmap;
+	    try
+	    {
+	        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
+	        // TODO : rotate image
+	        // bitmap = ExifUtil.rotateBitmap(mCurrentPhotoPath, bitmap);
+	        scaledBitmap = Bitmap.createScaledBitmap(bitmap, 200, 300, true);
+	        Log.d(TAG, "Photo is saved in " + mCurrentPhotoPath);
+	        return scaledBitmap;
+	    }
+	    catch (Exception e)
+	    {
+	        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+	        Log.d(TAG, "Failed to load", e);
+	    }
+	    return null;
+	}
+
+	//called after camera intent finished
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+	    if(requestCode==REQUEST_TAKE_PHOTO && resultCode==RESULT_OK)
+	    {
+	        // Rescan gallery to fetch newly added photo
+	        sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED,
+	                Uri.parse("file://"
+	                        + Environment.getExternalStorageDirectory())));
 	        Intent intent = new Intent(this, CreateMissionActivity.class);
-	        Bundle extras = data.getExtras();
-	        Bitmap imageBitmap = (Bitmap) extras.get("data");
-	        intent.putExtra("picture", imageBitmap);
+	        Bitmap bitmap = grabImage();
+	        intent.putExtra("picture", bitmap);
 	        startActivity(intent);
 	    }
+	    super.onActivityResult(requestCode, resultCode, data);
 	}
 }
